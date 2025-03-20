458129c3ed98425a82b8eb129847e90d144a3a18
Hiridhar-11 <hiridhar.3.3@protosem.tech>
2025-03-20 10:41:57 +0530

    Update script.js

--- assests/js/script.js
+++ assests/js/script.js
@@ -10,12 +10,12 @@ document.addEventListener("DOMContentLoaded", function () {
     const texts = ["Electrical Engineer", "Innovative Thinker", "UI/UX Developer"];
     let index = 0;
     const typingEffect = document.querySelector(".typing-effect");
-    
+
     function typeText() {
         let text = texts[index];
         let i = 0;
         typingEffect.textContent = "";
-        
+
         function typeLetter() {
             if (i < text.length) {
                 typingEffect.textContent += text[i];
@@ -25,7 +25,7 @@ document.addEventListener("DOMContentLoaded", function () {
                 setTimeout(eraseText, 2000);
             }
         }
-        
+
         function eraseText() {
             if (typingEffect.textContent.length > 1) {
                 typingEffect.textContent = typingEffect.textContent.slice(0, -1);
@@ -35,8 +35,10 @@ document.addEventListener("DOMContentLoaded", function () {
                 setTimeout(typeText, 500);
             }
         }
-        
+
         typeLetter();
     }
+
     typeText();
 });
+
